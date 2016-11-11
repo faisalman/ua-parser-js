97b52b2cfd94933050df1f76a8e447883e8b8f10
Faisal Salman <fyzlman@gmail.com>
2016-11-11 13:58:15 +0700

    Test unit for extension

--- test/test.js
+++ test/test.js
@@ -43,15 +43,6 @@ describe('UAParser()', function () {
     assert.deepEqual(UAParser(ua), new UAParser().setUA(ua).getResult());
 });
 
-describe('Injected Browser', function () {
-    var uaString = 'ownbrowser/1.3';
-    var ownBrowser = [[/(ownbrowser)\/((\d+)?[\w\.]+)/i], [UAParser.BROWSER.NAME, UAParser.BROWSER.VERSION, UAParser.BROWSER.MAJOR]];
-    var parser = new UAParser(uaString, {browser: ownBrowser});
-    assert.equal(parser.getBrowser().name, 'ownbrowser');
-    assert.equal(parser.getBrowser().major, '1');
-    assert.equal(parser.getBrowser().version, '1.3');
-});
-
 for (var i in methods) {
     describe(methods[i]['title'], function () {
         for (var j in methods[i]['list']) {
@@ -72,3 +63,12 @@ for (var i in methods) {
         }
     });
 }
+
+describe('Extending Regex', function () {
+    var uaString = 'Mozilla/5.0 MyOwnBrowser/1.3';
+    var myOwnBrowser = [[/(myownbrowser)\/((\d+)?[\w\.]+)/i], [UAParser.BROWSER.NAME, UAParser.BROWSER.VERSION, UAParser.BROWSER.MAJOR]];
+    var parser = new UAParser(uaString, {browser: myOwnBrowser});
+    assert.equal(parser.getBrowser().name, 'MyOwnBrowser');
+    assert.equal(parser.getBrowser().version, '1.3');
+    assert.equal(parser.getBrowser().major, '1');
+});
