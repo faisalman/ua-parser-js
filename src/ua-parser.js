7aca357879ba18ec2e57d36403d391c860a1be2e
Faisal Salman <fyzlman@gmail.com>
2018-10-26 01:35:15 +0700

    Fix #328 New release 0.7.19

--- src/ua-parser.js
+++ src/ua-parser.js
@@ -1,5 +1,5 @@
 /*!
- * UAParser.js v0.7.18
+ * UAParser.js v0.7.19
  * Lightweight JavaScript-based User-Agent string parser
  * https://github.com/faisalman/ua-parser-js
  *
@@ -16,7 +16,7 @@
     /////////////
 
 
-    var LIBVERSION  = '0.7.18',
+    var LIBVERSION  = '0.7.19',
         EMPTY       = '',
         UNKNOWN     = '?',
         FUNC_TYPE   = 'function',
