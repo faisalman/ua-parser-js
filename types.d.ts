bb06b86fe037a438171ba194c5dbf3af408c7ed7
jason-chang <chaoyue.chang@gmail.com>
2020-06-23 19:31:39 +0800

    fix definitions for extensions param, 	add definition for static properties.

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,35 @@
 
 declare module 'ua-parser-js' {
+    type UAModelKey = 'model';
     type UANameKey = 'name';
+    type UATypeKey = 'type';
+    type UAVendorKey = 'vendor';
     type UAVersionKey = 'version';
+    type UAArchitectureKey = 'architecture';
+    type UAConsoleKey = 'console';
+    type UAMobileKey = 'mobile';
+    type UATabletKey = 'tablet';
+    type UASmarttvKey = 'smarttv';
+    type UAWearableKey = 'wearable';
+    type UAEmbeddedKey = 'embedded';
 
-    type UAParserExtensions = Array<RegExp[] | [UANameKey | [UANameKey, string], UAVersionKey]>;
+    type UAParserExtensions = {
+        browser: Array<RegExp[] | Array<UANameKey | [UANameKey, string] | UAVersionKey>>,
+        cpu: Array<RegExp[] | Array<UAArchitectureKey | [UAArchitectureKey, string] | [UAArchitectureKey, (str: string) => string]>>
+        device: Array<RegExp[] | Array<
+            UAModelKey | [UAModelKey, string] | [UAModelKey, (str: string, map: { [key: string]: string | string[] }) => string, { [key: string]: string | string[] }]
+            | UAVendorKey | [UAVendorKey, string]
+            | [UATypeKey, UATabletKey | UASmarttvKey | UAMobileKey | UAConsoleKey | UAWearableKey]
+            >>
+        engine: Array<RegExp[] | Array<
+            UAVendorKey | [UANameKey, string]
+            >>,
+        os: Array<RegExp[] | Array<
+            UANameKey | [UANameKey, string]
+            | UAVersionKey | [UAVersionKey, string]
+            | [UAVersionKey, (str: string, map: { [key: string]: string | string[] }) => string, { [key: string]: string | string[] }]
+            >>,
+    };
 
     type UABrowserResult = { name: string, version: string };
     type UADeviceResult = { model: string, type: string, vendor: string };
@@ -11,22 +37,53 @@ declare module 'ua-parser-js' {
     type UAOSResult = { name: string, version: string };
     type UACpuResult = { architecture: string };
 
-    interface UAParser {
-        new(): UAParser;
-        new(uastring: string): UAParser;
-        new(extensions: UAParserExtensions): UAParser;
-        new(uastring: string, extensions: UAParserExtensions): UAParser;
+    class UAParser {
+        constructor();
+        constructor(uastring: string);
+        constructor(extensions: UAParserExtensions);
+        constructor(uastring: string, extensions: UAParserExtensions);
         getBrowser(): UABrowserResult;
         getDevice(): UADeviceResult;
         getEngine(): UAEngineResult;
         getOS(): UAOSResult;
         getCPU(): UACpuResult;
-        getResult(): { ua: string, browser: UABrowserResult, cpu: UACpuResult, device: UADeviceResult, engine: UAEngineResult, os: UAOSResult }
-        getUA(): string,
-        setUA(uastring: string): void,
+        getResult(): { ua: string, browser: UABrowserResult, cpu: UACpuResult, device: UADeviceResult, engine: UAEngineResult, os: UAOSResult };
+        getUA(): string;
+        setUA(uastring: string): void;
+
+        static VERSION: string;
+
+        static BROWSER: {
+            NAME: UANameKey,
+            VERSION: UAVersionKey,
+        };
+
+        static CPU: {
+            ARCHITECTURE : UAArchitectureKey,
+        };
+
+        static DEVICE: {
+            MODEL: UAModelKey,
+            VENDOR: UAVendorKey,
+            TYPE: UATypeKey,
+            CONSOLE: UAConsoleKey,
+            MOBILE: UAMobileKey,
+            SMARTTV: UASmarttvKey,
+            TABLET: UATabletKey,
+            WEARABLE: UAWearableKey,
+            EMBEDDED: UAEmbeddedKey
+        };
+
+        static ENGINE: {
+            NAME: UANameKey,
+            VERSION: UAVersionKey,
+        };
+
+        static OS: {
+            NAME: UANameKey,
+            VERSION: UAVersionKey,
+        };
     }
 
-    const uap: UAParser;
-
-    export default uap;
+    export default UAParser;
 }
