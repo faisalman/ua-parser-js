23ad60ea689e2d25ec9e403bf128d3b974c341cb
Faisal Salman <f@faisalman.com>
2021-12-18 01:31:40 +0700

    Fix #559: Sony Xperia 1 III misidentified as Acer tablet

--- src/ua-parser.js
+++ src/ua-parser.js
@@ -449,7 +449,7 @@
             ], [MODEL, [VENDOR, GOOGLE], [TYPE, MOBILE]], [
 
             // Sony
-            /droid.+ ([c-g]\d{4}|so[-gl]\w+|xq-a\w[4-7][12])(?= bui|\).+chrome\/(?![1-6]{0,1}\d\.))/i
+            /droid.+ (a?\d[0-2]{2}so|[c-g]\d{4}|so[-gl]\w+|xq-a\w[4-7][12])(?= bui|\).+chrome\/(?![1-6]{0,1}\d\.))/i
             ], [MODEL, [VENDOR, SONY], [TYPE, MOBILE]], [
             /sony tablet [ps]/i,
             /\b(?:sony)?sgp\w+(?: bui|\))/i
