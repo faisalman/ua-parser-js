a6140a17dd0300a35cfc9cff999545f267889411
Faisal Salman <f@faisalman.com>
2023-01-20 15:03:31 +0700

    Remove unsafe regex in trim() function
    `trim()` function contains a regular expression that is vulnerable to ReDoS but was uncaught by `safe-regex` module.

--- src/ua-parser.js
+++ src/ua-parser.js
@@ -93,7 +93,7 @@
         },
         trim = function (str, len) {
             if (typeof(str) === STR_TYPE) {
-                str = str.replace(/^\s\s*/, EMPTY).replace(/\s\s*$/, EMPTY);
+                str = str.replace(/^\s\s*/, EMPTY);
                 return typeof(len) === UNDEF_TYPE ? str : str.substring(0, UA_MAX_LENGTH);
             }
     };
