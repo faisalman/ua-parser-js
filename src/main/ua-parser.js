f0edfacff6b5f44b3a6d451e9ad88604299b3222
Andreas Kogler <andy.kogler.junior@gmail.com>
2023-04-26 15:59:27 +0200

    Add support for `WebMAF` detection on Playstation

--- src/main/ua-parser.js
+++ src/main/ua-parser.js
@@ -285,6 +285,7 @@
             // Mixed
             /(kindle)\/([\w\.]+)/i,                                             // Kindle
             /(lunascape|maxthon|netfront|jasmine|blazer)[\/ ]?([\w\.]*)/i,      // Lunascape/Maxthon/Netfront/Jasmine/Blazer
+            /(webmaf)\/(\S+)/i,                                                 // Sony/Playstation WebMAF
             // Trident based
             /(avant |iemobile|slim)(?:browser)?[\/ ]?([\w\.]*)/i,               // Avant/IEMobile/SlimBrowser
             /(ba?idubrowser)[\/ ]?([\w\.]+)/i,                                  // Baidu Browser
