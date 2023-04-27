102dc51683f9901028e3c450e84acd01c6db0032
Faisal Salman <f@faisalman.com>
2023-04-27 12:36:52 +0700

    Update fuzzing test

--- test/jazzer-fuzz-test.js
+++ test/jazzer-fuzz-test.js
@@ -1,9 +1,14 @@
+const { FuzzedDataProvider } = require('@jazzer.js/core');
 const UAParser = require('ua-parser-js');
+const UA_MAX_LENGTH = 350;
 
 module.exports.fuzz = function (buffer) {
-    const userAgent = buffer.toString();
+    const data = new FuzzedDataProvider(buffer);
+    const userAgent = data.consumeString(UA_MAX_LENGTH, 'utf-8', true);
     const start = process.hrtime();
+    
     UAParser(userAgent);
+
     const elapsed = process.hrtime(start);
     const milisec = (elapsed[0]*1e3+elapsed[1]*1e-6).toFixed(3);
     if (milisec > 1000) {
