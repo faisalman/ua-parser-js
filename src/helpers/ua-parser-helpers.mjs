73a936001a96e8d1f6be847ec169b6574f7664c4
Faisal Salman <f@faisalman.com>
2023-08-21 11:28:30 +0700

    Modify `ua-parser-js/helpers` submodule into `@ua-parser-js/helpers` scoped package

--- src/helpers/ua-parser-helpers.mjs
+++ src/helpers/ua-parser-helpers.mjs
@@ -3,7 +3,7 @@
 // Source: /src/helpers/ua-parser-helpers.js
 
 ///////////////////////////////////////////////
-/*  Helpers for UAParser.js v2.0.0-alpha.3
+/*  Helpers for UAParser.js
     https://github.com/faisalman/ua-parser-js
     Author: Faisal Salman <f@faisalman.com>
     MIT License */
@@ -37,9 +37,7 @@
     - "" (empty string for Tablets & Desktop)
 */
 
-const frozenUA = /Mozilla\/5\.0 \((Windows NT 10\.0; Win64; x64|Macintosh; Intel Mac OS X 10_15_7|X11; Linux x86_64|X11; CrOS x86_64 14541\.0\.0|Fuchsia|Linux; Android 10; K)\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.0\.0\.0 (Mobile )?Safari\/537\.36/;
-
-const isFrozenUA = str => frozenUA.test(str);
+const isFrozenUA = ua => /Mozilla\/5\.0 \((Windows NT 10\.0; Win64; x64|Macintosh; Intel Mac OS X 10_15_7|X11; Linux x86_64|X11; CrOS x86_64 14541\.0\.0|Fuchsia|Linux; Android 10; K)\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.0\.0\.0 (Mobile )?Safari\/537\.36/.test(ua);
 
 export { 
     isFrozenUA
