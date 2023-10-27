d30ad469927590b8d59a37eb3a84af89c00e489a
Faisal Salman <f@faisalman.com>
2023-10-27 09:58:43 +0700

    Bump version 0.7.37

--- src/ua-parser.js
+++ src/ua-parser.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////////
-/* UAParser.js v0.7.36
+/* UAParser.js v0.7.37
    Copyright © 2012-2021 Faisal Salman <f@faisalman.com>
    MIT License *//*
    Detect Browser, Engine, OS, CPU, and Device type/model from User-Agent data.
@@ -17,7 +17,7 @@
     /////////////
 
 
-    var LIBVERSION  = '0.7.36',
+    var LIBVERSION  = '0.7.37',
         EMPTY       = '',
         UNKNOWN     = '?',
         FUNC_TYPE   = 'function',
