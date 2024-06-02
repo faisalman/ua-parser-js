5a8ce350548142081e60e3e2cdf79fb71206254a
Faisal Salman <f@faisalman.com>
2024-06-02 10:38:24 +0700

    Insert spaces to command line output for readability

--- script/cli.js
+++ script/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const UAParser = require('ua-parser-js');
-console.log(JSON.stringify(process.argv.slice(2).map(ua => UAParser(ua))));
+console.log(JSON.stringify(process.argv.slice(2).map(ua => UAParser(ua)), null, 4));
